ac8ad60a5c55b38472559ee097b193c2e0b10696
Puente Scapolatempo Juan Jose <juanpuentescapolatempo@gmail.com>
2022-05-02 20:00:40 -0300

    dummie commit

--- src/constants/STYLES.js
+++ src/constants/STYLES.js
@@ -6,4 +6,5 @@ export const COLOR_GRAY3 = "#606060"
 
 export const COLOR_BLUE1 = "#2F80ED"
 
-export const LIGHT_SHADOW = "0px 0px 32px -11px rgba(0,0,0,.4)"
+// export const LIGHT_SHADOW = "0px 0px 32px -11px rgba(0,0,0,.4)"
+export const LIGHT_SHADOW = "0px 0px 32px -11px rgba(0,0,100,.4)"
